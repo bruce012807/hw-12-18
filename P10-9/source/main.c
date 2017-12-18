c6c9ad412b6f76b82971a851f9481f965130f2f2
bruce012807 <bruce012807@gmail.com>
2017-12-18 22:24:52 +0800

    fuck

---
+++ P10-9/source/main.c
@@ -0,0 +1,61 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+struct card {
+	const char *face;
+	const char *suit;
+};
+
+typedef struct card card;
+
+void filldeck(card * const wdeck, const char *wface[], const char wsuit[]);
+void shuffle(card * const wdeck);
+void deal(const card * const wdeck);
+
+int main(void)
+{
+	card deck[52];
+	const char *face[] = { "ace","Deuce","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Jack","Queen","King" };
+	const char *suit[] = {"Hearts","Diamonds","Clubs","Spades"};
+
+	srand(time(NULL));
+
+	filldeck(deck, face, suit);
+	shuffle(deck);
+	deal(deck);
+
+	system("pause");
+	return 0;
+}
+
+void filldeck(card * const wdeck, const char *wface[], const char wsuit[])
+{
+	int i;
+	for (i = 0; i <= 51; i++)
+	{
+		wdeck[i].face = wface[i % 13];
+		wdeck[i].suit = wface[i / 13];
+	}
+}
+
+void shuffle(card * const wdeck)
+{
+	int i, j;
+	card temp;
+	for (i = 0; i <= 51; i++)
+	{
+		j = rand() % 52;
+		temp = wdeck[i];
+		wdeck[i] = wdeck[j];
+		wdeck[j] = temp;
+	}
+}
+void deal(const card * const wdeck)
+{
+	int i;
+	for (i = 0; i <= 51; i++)
+	{
+		printf("%5s of %-8s%s", wdeck[i].face, wdeck[i].suit, (i + 1) % 4 ? "  " : "\n");
+	}
+}
